2a065f2265f035e618506c8222ab44223965ec0c
Pedro Luiz de Moraes Ferreira <11700675656@dc01.ifs.ifsuldeminas.edu.br>
2022-06-23 09:08:52 -0300

    Projeto Inicial

---
+++ controller/Categoria.php
@@ -0,0 +1,11 @@
+<?php
+
+require "../model/CategoriaModel.php";
+
+$categoria = new CategoriaModel();
+$categoria->inserir("SmartTV");
+$categoria->excluir(1);
+$categoria->atualizar(4, "SmartPhone");
+var_dump($categoria->buscarPorId(3));
+var_dump($categoria->buscarTudo());
+
